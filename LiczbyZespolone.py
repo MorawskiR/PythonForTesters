da9e66cf847a0174f5691741b00a70732d6c6dd5
MorawskiR <r.morawski92@gmail.com>
2024-01-02 14:39:26 +0100

    files import

---
+++ LiczbyZespolone.py
@@ -0,0 +1,14 @@
+class Complex():
+    def __init__(self, pRe=0, pIm=0): # Konstruktor
+        self.__re = pRe # Tworzymy atrybut __re – część rzeczywista
+        self.__im = pIm # Tworzymy atrybut __im – część urojona
+    def __str__(self): # Zwracamy obiekty jako napis
+        return str(self.__re) + "+" + str(self.__im) + "*i"
+    def __add__(self, x2): # Realizacja operatora + (suma dwóch liczb zespolonych)
+        return Complex(self.__re + x2.__re, self.__im + x2.__im)
+    @property
+    def Re(self):
+        return self.__re
+    @property
+    def Im(self):
+        return self.__im
