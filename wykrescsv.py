36dea633b0a94cb9d87d8f59641701c31bf57fc5
MorawskiR <r.morawski92@gmail.com>
2024-01-19 21:13:38 +0100

    files

---
+++ wykrescsv.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+osX, osY, osY2 = np.loadtxt('danedowykresu.csv',
+delimiter=',', unpack=True)
+seria1,=plt.plot(osX, osY, marker='*', linestyle='--', color='k',
+ms=10, linewidth = '1', label='Seria 1.') # Seria 1.
+seria2,=plt.plot(osX, osY2, marker='o', linestyle='-', color='k',
+ms=10, linewidth = '1', label='Seria 2.') # Seria 2.
+plt.title("Pomiary napięcia\n(dane pobrane z pliku CSV)")
+plt.legend(handles=[seria1, seria2])
+plt.xlabel("[t]")
+plt.ylabel("[V]")
+plt.legend()
+plt.show()
